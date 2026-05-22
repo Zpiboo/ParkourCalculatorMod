ccf5b43a0cdfae86e0d2ffd51992af0894e76c44
Legoshi <46093332+Leg0shii@users.noreply.github.com>
2026-05-22 14:34:29 +0200

    fix: soft collision (#52)
    Closes #51

--- loader-fabric-1.21.10/src/client/java/de/legoshi/parkourcalc/fabric/sim/SimulatorEntity.java
+++ loader-fabric-1.21.10/src/client/java/de/legoshi/parkourcalc/fabric/sim/SimulatorEntity.java
@@ -228,6 +228,23 @@ public class SimulatorEntity extends PlayerEntity {
     }
 
     @Override
+    protected boolean hasCollidedSoftly(Vec3d adjustedMovement) {
+        float f = this.getYaw() * (float) (Math.PI / 180.0);
+        double d = MathHelper.sin(f);
+        double e = MathHelper.cos(f);
+        double g = this.sidewaysSpeed * e - this.forwardSpeed * d;
+        double h = this.forwardSpeed * e + this.sidewaysSpeed * d;
+        double i = MathHelper.square(g) + MathHelper.square(h);
+        double j = MathHelper.square(adjustedMovement.x) + MathHelper.square(adjustedMovement.z);
+        if (i < 1.0E-5F || j < 1.0E-5F) {
+            return false;
+        }
+        double k = g * adjustedMovement.x + h * adjustedMovement.z;
+        double l = Math.acos(k / Math.sqrt(i * j));
+        return l < 0.13962634F;
+    }
+
+    @Override
     public void tickMovementInput() {
         Vec2f movement = applyMovementSpeedFactors(this.input.getMovementInput());
         this.sidewaysSpeed = movement.x;
